5294071b1fa9a0b005bfd52f95a8db2caec99165
tinu bose <tinubose.mca2022@gmail.com>
2023-10-23 15:30:55 +0530

    retrieving values from list

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: My_Id(),
+      home: QuoteList(),
     ));
 
-class My_Id extends StatelessWidget {
-  My_Id({super.key});
+class QuoteList extends StatefulWidget {
+  QuoteList({super.key});
 
-  int age = 0;
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'you dont deserves me',
+    'you dont deserves my vibe',
+    'you dont deserves what i\'ll become'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text("ID CARD"),
-        backgroundColor: Colors.purple,
+        title: Text("Awesome Quotes"),
+        backgroundColor: Colors.orange,
         centerTitle: true,
-        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(({
-            age+=1;
-          }));
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.purple,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/me.jpeg'),
-                radius: 60,
-              ),
-            ),
-            Divider(
-              height: 90,
-              color: Colors.purple,
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(color: Colors.black, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "TINU BOSE",
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "AGE",
-              style: TextStyle(color: Colors.black, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '$age',
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.email_rounded,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "ttinubose@gmail.com",
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: Colors.black,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.phone,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "8921584423",
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: Colors.black,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) {
+          return Text(quote);
+        }).toList(),
       ),
     );
   }
